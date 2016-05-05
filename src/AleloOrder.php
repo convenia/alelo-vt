ec944ef6913e6b5f84b17f6889a2691e391a2134
Eduardo Bizarro <edbizarro@gmail.com>
2016-05-05 19:01:30 -0300

    tests improvements

--- src/AleloOrder.php
+++ src/AleloOrder.php
@@ -3,6 +3,8 @@
 namespace Edbizarro\AleloOrder;
 
 use Edbizarro\AleloOrder\Interfaces\AleloOrderInterface;
+use Edbizarro\AleloOrder\Registries\HeaderRegistry;
+use Stringy\Stringy;
 
 /**
  * Class AleloOrder.
@@ -10,11 +12,39 @@ use Edbizarro\AleloOrder\Interfaces\AleloOrderInterface;
 class AleloOrder implements AleloOrderInterface
 {
     /**
+     * @var Stringy
+     */
+    protected $fileLayout;
+
+    /**
+     * @var HeaderRegistry
+     */
+    protected $header;
+
+    /**
+     * Array of EmployeeRegisty
+     *
+     * @var array
+     */
+    protected $employees = [];
+
+    /**
+     * AleloOrder constructor.
+     */
+    public function __construct()
+    {
+        $this->fileLayout = Stringy::create('');
+    }
+
+    /**
      * Generate the alelo orders file.
      *
      * @return string
      */
     public function generate()
     {
+        $this->fileLayout = $this->fileLayout->append();
+
+        return (string) $this->fileLayout;
     }
 }
--- tests/AleloOrder/Registries/HeaderRegistryTest.php
+++ tests/AleloOrder/Registries/HeaderRegistryTest.php
@@ -13,25 +13,13 @@ class HeaderRegistryTest extends BaseTest
     /**
      * @expectedException \Edbizarro\AleloOrder\Exceptions\FieldNotExistsException
      * @throws \Edbizarro\AleloOrder\Exceptions\FieldNotExistsException
+     * @throws \Edbizarro\AleloOrder\Exceptions\RegistryTooLongException
      */
     public function test_field_not_exists()
     {
         new HeaderRegistry([
             'invalid_field' => '1'
         ]);
-    }
-
-    public function test_fields()
-    {
-        $header = new HeaderRegistry([
-            'orderDate' => '05052016',
-            'name' => 'Razão',
-
-        ]);
-
-        $expected = '005052016A001RAZAO                              0000000000000000000000000000000000000000000000000000000000                  007                                                                                                                                                                                                                                                                           000000';
-
-        $this->assertEquals($expected, (string)$header);
     }
 
     public function test_complete_fields()
